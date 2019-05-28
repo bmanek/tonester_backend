7cb75abff130613caae28863efefb414b4bb0b75
bmanek <41875460+bmanek@users.noreply.github.com>
2019-05-28 18:51:03 -0400

    end of day

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,23 @@ class UsersController < ApplicationController
    render json: User.all, include: '**'
 
   end
-  
-  def login 
-    
-  
-  end 
+
+  def login
+    if params[:username] == User.find_by(username: params[:username]).username
+      user = User.find_by(username: params[:username]).id
+      render json: {user_id: user}
+    else
+      render json: {message: "try harder, buddy!"}
+    end
+  end
 
   def show
-    
+
     render json: User.find(params[:id])
   end
 
   def create
-  
+
     render json: User.find_or_create_by(user_params)
   end
 
