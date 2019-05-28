079986ec7210be7b354665c872e7c05fca16efb4
bmanek <41875460+bmanek@users.noreply.github.com>
2019-05-28 11:05:55 -0400

    add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
   def index
-
+    # byebug
+    render json: User.all
   end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    byebug
+    render json: User.find_or_create_by(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:bio, :photo, :username)
+  end
+
 end
