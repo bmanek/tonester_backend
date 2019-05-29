3702e5f5682bafd39545e22b3e7d68900d9e9fad
bmanek <41875460+bmanek@users.noreply.github.com>
2019-05-29 16:50:23 -0400

    refactor create method to support dynamic page reload

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,17 @@
 class SongsController < ApplicationController
-    def search 
+
+    def index
+      render json: Song.all
+    end
+
+    def create
         newSong = Song.find_by(title: params[:title])
         if newSong
-        render json: Song.find(newSong.id)
+          render json: newSong
+          PlaylistSong.find_or_create_by(song_id: newSong.id, playlist_id: params[:playlist_id] )
         else
-        render json: {message: "Guess its is not here yet!"}    
+          render json: {message: "Guess its is not here yet!"}
         end
     end
+
 end
